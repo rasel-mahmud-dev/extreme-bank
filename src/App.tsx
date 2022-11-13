b17eda068d1d2ae13038e23db6ff84128b8e7826
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-11-14 00:14:29 +0600

    project initialize with solidjs and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+
+const App: Component = () => {
+  return (
+    <div >
+      <h1 class="text-green-500">Helllo</h1>
+    </div>
+  );
+};
+
+export default App;
